fa8a870568af26e5a54bf078bcc81587ffed12cd
TimW010 <0981877@hr.nl>
2021-05-31 11:05:57 +0200

    Added endscreen functionality

--- CLE_4_Prototypes/Letter_enveloppen/dev/endscreen.ts
+++ CLE_4_Prototypes/Letter_enveloppen/dev/endscreen.ts
@@ -1,7 +1,15 @@
 export class EndScreen {
+
+    screen : HTMLElement;
+
+
     constructor(){
         console.log('ik ben het eindscherm.');
-    }
+        this.screen = document.getElementById('Screen')!
+        this.showEndScreen();
+    }   
 
-    
+    private showEndScreen(){
+        this.screen.innerText = "Goed Gedaan!";
+    }
 }
